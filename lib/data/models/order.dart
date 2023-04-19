fdb1cd6a38a8bc57cd809321ecf7b30424dfdeee
Cecilia Ha <hamytrinh9a1@gmail.com>
2023-04-19 20:10:10 +0700

    order

---
+++ lib/data/models/order.dart
@@ -0,0 +1,44 @@
+import 'package:foody/data/models/cart_item.dart';
+
+class Order {
+  String orderID;
+  String orderDate;
+  int orderSum;
+  int orderQuantity;
+  String userID;
+
+  List<CartItem>? orderFood;
+
+  Order(
+      {required this.orderID,
+      required this.orderDate,
+      required this.orderSum,
+      required this.orderQuantity,
+      required this.userID,
+      this.orderFood});
+
+  factory Order.fromJson(Map map) {
+    return Order(
+        orderID: map["orderID"],
+        orderDate: map["orderDate"],
+        orderSum: map["orderSum"] as int,
+        orderQuantity: map["orderQuantity"] as int,
+        userID: map["userID"],
+        orderFood: map["orderFood"] != null
+            ? (map["orderFood"] as List)
+                .map((e) => CartItem.fromMap(e))
+                .toList()
+            : []);
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      "orderID": orderID,
+      "orderDate": orderDate,
+      "orderSum": orderSum,
+      "orderQuantity": orderQuantity,
+      "userID": userID,
+      "orderFood": orderFood?.map((e) => e.toMap())
+    };
+  }
+}
